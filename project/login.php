da54c2a58a07af951a85323fcf2ef493d3e5a208
rhrastic <ryanhrastich@yahoo.com>
2023-01-10 10:17:34 -0600

    Add files via upload

---
+++ project/login.php
@@ -0,0 +1,21 @@
+<?php
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$captcha = $_POST['captcha'];
+
+	// Database connection, table title will need to change
+	$conn = new mysqli('localhost','root','','database');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into login(username, password, captcha) values(?, ?, ?)");
+		$stmt->bind_param("sss", $username, $password, $captcha);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Join successful...";
+		header('Location: http://localhost/loginPage.html');
+		$stmt->close();
+		$conn->close();
+	}
+?>
