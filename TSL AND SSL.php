4925a39bfb6d2b2bc799ddf0a2445425f0b0fca3
aj0192823 <121986434+aj324324@users.noreply.github.com>
2023-01-21 14:23:24 -0600

    Create TSL AND SSL.php

---
+++ TSL AND SSL.php
@@ -0,0 +1,8 @@
+<?php
+// redirect users to the HTTPS version of the site.
+if ($_SERVER['HTTPS'] != "on") {
+    $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    header("Location: $url");
+    exit;
+}
+?>
