fe48351adb7c5c2098f6785ddf8bd1a2c9af732c
aj0192823 <121986434+aj324324@users.noreply.github.com>
2023-01-25 12:03:07 -0600

    Delete TSL AND SSL.php

--- TSL AND SSL.php
+++
@@ -1,8 +0,0 @@
-<?php
-// redirect users to the HTTPS version of the site.
-if ($_SERVER['HTTPS'] != "on") {
-    $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-    header("Location: $url");
-    exit;
-}
-?>
